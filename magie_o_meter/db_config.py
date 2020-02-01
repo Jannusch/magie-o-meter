50221ae94f52210dbde0023f7e4e11032a94cbd5
Jannusch Bigge <jannusch.bigge@mailbox.tu-dresden.de>
2020-02-01 21:35:15 +0100

    postgresql

--- magie_o_meter/db_config.py
+++ magie_o_meter/db_config.py
@@ -1,24 +1,39 @@
-import sqlite3
-conn = sqlite3.connect('magie.db')
+import psycopg2
 
-c = conn.cursor()
-
-# c.execute('''CREATE TABLE magieValues
-#                (date text, magie text, impuls text, bw text)''')
-
-# Insert a row of data
-c.execute("INSERT INTO magieValues VALUES ('2020-01-30','12','49','29')")
-
-# Save (commit) the changes
-conn.commit()
-
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
+def inster_date_to_database(date_value, magie_value, impuls_value, bw_value):
+    command = f"INSERT INTO values(date, magie, impuls, bw) VALUES (DATE '{date_value}', {magie_value}, {impuls_value}, {bw_value});"
+    __execute_command(command)
 
 
+def __execute_command(command):
+    try:
+        connection = psycopg2.connect(user = "jannusch",
+                                      password = "",
+                                      host = "127.0.0.1",
+                                      port = "5432",
+                                      database = "MagieOMeter")
 
-t = ('2020-01-30')
-c.execute('SELECT * FROM magie_values')
-print(c.fetchone())
+        cursor = connection.cursor()
+        print(connection.get_dsn_parameters(), "\n")
 
-conn.close()
+        cursor.execute(command)
+        try:
+            record = cursor.fetchone()
+            print(f"You are connected to - {record} \n")
+        except (Exception, psycopg2.Error) as error:
+            print(f"Error while writing to PostgreSQL {error}")
+        finally:
+            connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+        print(f"Error while connecting to PostgreSQL {error}")
+    finally:
+        # closing database connection
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+
+if __name__ == "__main__":
+    inster_date_to_database('2020-01-01', 12, 13, 14)
