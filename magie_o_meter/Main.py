1e6c0336a1db26dee262bd48ca8e355a47e64a19
Jannusch Bigge <jannusch.bigge@mailbox.tu-dresden.de>
2020-02-08 22:40:02 +0100

    works, but got banned again

--- magie_o_meter/Main.py
+++ magie_o_meter/Main.py
@@ -1,14 +1,8 @@
+import time
+import urllib.request
+from magie_o_meter.XMLParser import get_values_from_html
 from magie_o_meter.db_config import inster_date_to_database
 from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from magie_o_meter.magie_o_meter.spiders.crawler import MagieSpider
-from twisted.internet import reactor
-import scrapy
-from scrapy.crawler import CrawlerRunner
-from scrapy.utils.log import configure_logging
-import os
-from twisted.internet.task import deferLater
-import time
 
 
 def main():
@@ -34,30 +28,33 @@ def main():
 
 
     for url in start_urls:
-        process = CrawlerProcess({
-            'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-        })
-
         date = ''
-        for i in range(59,67):
+        for i in range(59,69):
             date = date + url[i]
         time.sleep(5)
-        process.crawl(MagieSpider, url=url, date=date)
+        try:
+            html = request_data(url)
+        except:
+            print("Error scraping")
+            html = ""
 
-def sleep(self, *args, seconds):
-    """Non blocking sleep callback"""
-    return deferLater(reactor, seconds, lambda: None)
+        # html = open('website.html', "r")
+        values = get_values_from_html(html)
+
+        try:
+            inster_date_to_database(date, values[0], values[1], values[2])
+        except:
+            print("Insert Error")
+            inster_date_to_database(date, 0,0,0)
 
 
-process = CrawlerProcess(get_project_settings())
-
-
-def _crawl(result, spider):
-    deferred = process.crawl(spider)
-    deferred.addCallback(lambda results: print('waiting 100 seconds before restart...'))
-    deferred.addCallback(sleep, seconds=100)
-    deferred.addCallback(_crawl, spider)
-    return deferred
+def request_data(url):
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:48.0) Gecko/20100101 Firefox/48.0"
+    req = urllib.request.Request(url, headers=headers)
+    html = urllib.request.urlopen(req).read()
+    # print(html)
+    return html
 
 if __name__ == "__main__":
     main()
